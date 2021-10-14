604ba29f1631004b3da7b9923117f486e330b8b9
francocalluso <francocalluso@gmail.com>
2021-10-14 12:23:01 -0300

    agregado de objeto paciente

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 
-function solicitarInfo() {
+function paciente(nombre, tipoDeTurno, telefono) {
+    this.nombre=nombre;
+    this.tipoDeTurno = tipoDeTurno;
+    this.telefono = telefono;
+    }
+
 let nombrePaciente = prompt("Escribe el nombre y apellido del paciente");
 let prepaga = prompt(" Bienvenido/a " + nombrePaciente + " ¿Tienes OSDE?");
-
 if (prepaga.toLowerCase() == "si") {
     let numeroDeSocio= parseInt(prompt("indica tu número de socio de OSDE"))
 }
+let tipoDeTurno = prompt("Escribe el tipo de turno. \n 1- Consulta 1era vez \n 2- Control de aparatología fija \n 3- Control de aparatología móvil \n 4- Toma de moldes \n 5- Cementado \n 6- Retiro de aparatología")
 
 let numeroTelefonico = parseInt(prompt ( "Escribe tu numero de teléfono. Lo usaremos para confirmar tu turno."));
 
 alert ("Gracias "+ nombrePaciente + " nos comunicaremos a la brevedad al número " +numeroTelefonico + " para confirmar tu turno." )
-}
 
-solicitarInfo()
+const paciente1 = new paciente (nombrePaciente, tipoDeTurno, numeroTelefonico);
+
+console.log (paciente1);
+
+
+
 
 
 
