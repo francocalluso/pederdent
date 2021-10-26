f3c3f79c98a4fc4a3ce40266afe9e2ced4a13429
francocalluso <francocalluso@gmail.com>
2021-10-26 12:34:00 -0300

    primera aproximación a DOM

--- js/script.js
+++ js/script.js
@@ -42,47 +42,18 @@ listaPacientes.sort ((p1, p2)=> {
 });
 console.log(listaPacientes);
 
+let tituloLista = document.createElement("h2");
+tituloLista.innerHTML= `LISTA DE PACIENTES INGRESADOS`
+document.getElementById("listaPacientesIngresados").appendChild(tituloLista);
 
+for (const paciente of listaPacientes) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `
+                            <h3> Apellido: ${paciente.apellido}  <br>  Nombre: ${paciente.nombre}</h3>
+                            <p> Teléfono: ${paciente.telefono}<br>
+                            Tipo de turno: ${paciente.tipoDeTurno}<br>
+                             OSDE: ${paciente.osde}</p>`;
+    
+    document.getElementById("listaPacientesIngresados").appendChild(contenedor)
+}
 
-//ACA ARRANCA ARRAY
-
-// class pacientes {
-//     constructor (nombre, edad, prepaga) {
-//         this.nombre = nombre.toUpperCase();
-//         this.edad = edad;
-//         this.prepaga = prepaga;
-//     }
-// }
-
-// const paciente1 = new pacientes("Franco Calluso", 33, false);
-// const paciente2 = new pacientes("Ramiro Ochoa", 21, true);
-// const paciente3 = new pacientes("Sofia Gugliera",16, true);
-// const paciente4 = new pacientes(prompt("Ingrese nombre del paciente a ingresar"), parseInt(prompt("Ingrese su edad")), prompt("¿Tiene OSDE?"));
-
-
-// const listaPacientes = [];
-// listaPacientes.push(paciente1);
-// listaPacientes.push(paciente2);
-// listaPacientes.push(paciente3);
-// listaPacientes.push(paciente4);
-
-// listaPacientes.sort ((p1, p2)=> {
-//     if (p1.nombre < p2.nombre) {
-//         return -1;
-//     } else if (p1.nombre > p2.nombre){
-//         return 1;
-//     } else {
-//         return 0;
-//     }
-// });
-// console.log(listaPacientes);
-
-// function buscarPaciente(ficha, nombre){
-//     let encontrado = ficha.find(paciente => paciente.nombre == nombre)
-//     if (encontrado == undefined){
-//         encontrado = "Paciente no encontrado";
-//     }
-//     return encontrado;
-// }
-// let resultado = buscarPaciente(listaPacientes, prompt("Ingrese el nombre del paciente a buscar").toUpperCase());
-// console.log (resultado);
